21ad6d847208f5ff444bf59e6a0da54fd771f4eb
unknown <cbrennan52>
2018-09-14 16:34:44 -0400

    yo

--- js/submitButton.js
+++ js/submitButton.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	$('#summernote').summernote({
+		placeholder : 'We can\'t wait to hear from you!'
+	});
 	var submitButton = $('#submitButton');
 	submitButton.click(function(e) {
 		e.preventDefault();
